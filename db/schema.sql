ed260f4068013a6701b6a42c5446c869f9a29981
Neverender <tldavis09@yahoo.com>
2020-03-22 19:55:56 -0500

    Adding folders

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (30),
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("CHUNGUS Burger", FALSE),
+    ("Nest Burger", TRUE),
+    ("Beet Kids Vegie Burger", TRUE)
