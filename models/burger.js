85f18922992dd7130c2de792f0d6188135cf79ef
Neverender <tldavis09@yahoo.com>
2020-03-23 00:08:20 -0500

    yep

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+const orm = require("../config/orm.js");
+
+// orm functions go here
+const burger = {
+    
+}
+
+
+module.exports = // name of the variable created that contains object of orm functions
