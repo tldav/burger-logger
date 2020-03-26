e8959466352d224e259b8c7865e0670d11797c08
Neverender <tldavis09@yahoo.com>
2020-03-25 19:08:36 -0500

    .

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+module.exports = router;
