964d949c6fb7f640f6b1b856f45d2885716274cf
Neverender <tldavis09@yahoo.com>
2020-03-25 19:19:23 -0500

    linking to heroku app

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql2");
+let connection;
 
-const connection = mysql.createConnection({
-	host: "localhost",
-	port: 3036,
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+// creates a connection w/ JawsDB if it contains a JawsDB env variable. Otherwise, connects to the local database
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3036,
+		user: "root",
+		password: "",
+		database: "burgers_db"
+	});
+}
+
+// Initiate Connection
 connection.connect((err) => {
 	if (err) {
 		console.error(`Error connecting: ${err.stack}`);
@@ -15,5 +23,7 @@ connection.connect((err) => {
 	}
 	console.log(`Connected as id ${connection.threadId}`);
 });
+
+// Export the connection
 
 module.exports = connection;
